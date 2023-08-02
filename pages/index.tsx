3e089ca58feb63358bac169bf7439dbebed3653b
JustAIGithub <enqueueit@gmail.com>
2023-08-02 08:12:46 +0800

    add sitemap

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import Head from 'next/head';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [title, setTitle] = useState('Code Translator');
+  const [subtitle, setSubtitle] = useState('Translate Code or Natural Language To Programming Language Code');
   const [inputLanguage, setInputLanguage] = useState<string>('Natural Language');
   const [outputLanguage, setOutputLanguage] = useState<string>('Python');
   const [inputCode, setInputCode] = useState<string>('');
@@ -97,19 +99,63 @@ export default function Home() {
   };
 
   useEffect(() => {
-	const isOutputLanguageInArray = languages.some(
+	  const isOutputLanguageInArray = languages.some(
 	      (language) => language.value === outputLanguage
-	);
-    if (hasTranslated && isOutputLanguageInArray) {
-      handleTranslate();
-    }
+	  );
+      if (hasTranslated && isOutputLanguageInArray) {
+        handleTranslate();
+      }
+	
+	  const currentPath = window.location.pathname;
+	  const convertRegex = /^\/convert-code-from-(\w+)-to-(\w+)$/;
+	  const translateRegex = /^\/translate-code-from-(\w+)-to-(\w+)$/;
+	  const generateRegex = /^\/generate-code-from-(\w+)-to-(\w+)$/;
+
+	  let fromLang = '';
+	  let toLang = '';
+	  let preName = '';
+	  let preAction = '';
+	  if (convertRegex.test(currentPath)) {
+		const match = currentPath.match(convertRegex);
+		if (match) {
+		  fromLang = match[1];
+		  toLang = match[2];
+		  preName = 'Code Convertor';
+		  preAction = 'Convert';
+		}
+	  } else if (translateRegex.test(currentPath)) {
+		const match = currentPath.match(translateRegex);
+		if (match) {
+		  fromLang = match[1];
+		  toLang = match[2];
+		  preName = 'Code Translator';
+		  preAction = 'Translate';
+		}
+	  } else if(generateRegex.test(currentPath)) {
+		  const match = currentPath.match(translateRegex);
+		  if (match) {
+		    fromLang = match[1];
+		    toLang = match[2];
+		    preName = 'Code Generator';
+		    preAction = 'Generate';
+		  }
+	  }
+
+	  if (fromLang && toLang) {
+		setTitle(`${preName}`);
+		setSubtitle(`${preAction} Code From ${fromLang} To ${toLang} Online Free With AI`);
+	  } else {
+		// 如果路径不匹配任何前缀，则设置默认文本内容
+		setTitle('Code Translator');
+		setSubtitle('Translate Code or Natural Language To Programming Language Code');
+	  }
   }, [outputLanguage]);
 
   return (
     <>
       <Head>
-        <title>AI Code Converter</title>
-        <meta name="description" content="Use AI to Convert Code or Generate Code from one language to another. AI Code Translator."/>
+        <title>AI Code Converter | AI Code Translator | AI Code Generator</title>
+        <meta name="description" content="Use AI To Convert Code Or Generate Code From One Language To Another. AI Code Translator. Translate Code From Any Language To Another With A Click Of A Button."/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="keywords" content="AI Code Converter,Code Convert AI, Code Generate AI,Code Translator,AICodeHelper,free,online" />
 		<link rel="canonical" href="https://aicodeconvert.com" />
@@ -131,15 +177,16 @@ export default function Home() {
       </Head>
 	  
 	  <div className="h-100 flex justify-start items-center pl-10 pt-2 bg-[#0E1117]">
-	  	<img className="w-50 h-50" alt="AICodeConverter" src="code.png" />
+	  	<svg width="40" height="40" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+	  	    <path fill="#3b82f6" d="M516 673c0 4.4 3.4 8 7.5 8h185c4.1 0 7.5-3.6 7.5-8v-48c0-4.4-3.4-8-7.5-8h-185c-4.1 0-7.5 3.6-7.5 8v48zm-194.9 6.1l192-161c3.8-3.2 3.8-9.1 0-12.3l-192-160.9A7.95 7.95 0 0 0 308 351v62.7c0 2.4 1 4.6 2.9 6.1L420.7 512l-109.8 92.2a8.1 8.1 0 0 0-2.9 6.1V673c0 6.8 7.9 10.5 13.1 6.1zM880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zm-40 728H184V184h656v656z"/>
+	  	</svg>
 	  	<h1 className="text-white font-bold text-2xl"><span className="text-blue-500 ml-2">AI</span>CodeConvert.com</h1>
 	  </div>
 	  
       <div className="flex h-full min-h-screen flex-col items-center bg-[#0E1117] px-4 pb-20 text-neutral-200 sm:px-10">
 		<div className="mt-2 flex flex-col items-center justify-center sm:mt-10">
-          <h2 className="text-4xl font-bold"><span className="text-blue-500">AI</span> Code Translator</h2>
-		  <div className="mt-5 text-xl text-center leading-2">Translate <span className="text-blue-500 font-bold">Code</span> or <span className="text-blue-500 font-bold">Natural Language</span> To
-		  Programming Language <span className="text-blue-500 font-bold">Code</span></div>
+          <h2 className="text-4xl font-bold"><span className="text-blue-500">AI</span> {title}</h2>
+		  <h3 className="mt-5 text-xl text-center leading-2">{subtitle}</h3>
         </div>
 		
         <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
@@ -217,7 +264,7 @@ export default function Home() {
 				<div 
 					className="items-center bg-repeat flex text-sm font-medium justify-center py-2 px-6 border border-solid rounded-full">
 					<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-					    <path fill="none" stroke="#6b7280" stroke-linecap="round" stroke-width="1.5" d="m17 7.83l1.697 1.526c1.542 1.389 2.313 2.083 2.313 2.974c0 .89-.771 1.585-2.314 2.973L17 16.83M13.987 5L12 12.415l-1.987 7.415M7 7.83L5.304 9.356C3.76 10.745 2.99 11.44 2.99 12.33c0 .89.771 1.585 2.314 2.973L7 16.83"/>
+					    <path fill="none" stroke="#6b7280" strokeLinecap="round" strokeWidth="1.5" d="m17 7.83l1.697 1.526c1.542 1.389 2.313 2.083 2.313 2.974c0 .89-.771 1.585-2.314 2.973L17 16.83M13.987 5L12 12.415l-1.987 7.415M7 7.83L5.304 9.356C3.76 10.745 2.99 11.44 2.99 12.33c0 .89.771 1.585 2.314 2.973L7 16.83"/>
 					</svg>
 					<p className="ml-2 text-white">Visit Base64.kr</p>
 				</div>
@@ -226,7 +273,7 @@ export default function Home() {
 				<div 
 					className="items-center bg-repeat flex text-sm font-medium justify-center py-2 px-6 border border-solid rounded-full">
 					<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-					    <path fill="none" stroke="#0ea5e9" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4v16M8 9v6m12-5v4M4 10v4m12-7v10"/>
+					    <path fill="none" stroke="#0ea5e9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 4v16M8 9v6m12-5v4M4 10v4m12-7v10"/>
 					</svg>
 					<p className="ml-2 text-white">Visit SpokenGPT</p>
 				</div>
