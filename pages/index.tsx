12cc0d0b97dfdd663bb7d6ff9afcda96a6ab2d2d
JustAIGithub <enqueueit@gmail.com>
2023-08-04 07:39:10 +0800

    add socal link

--- pages/index.tsx
+++ pages/index.tsx
@@ -224,8 +224,20 @@ export default function Home() {
 					<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 					    <path fill="none" stroke="#6b7280" strokeLinecap="round" strokeWidth="1.5" d="m17 7.83l1.697 1.526c1.542 1.389 2.313 2.083 2.313 2.974c0 .89-.771 1.585-2.314 2.973L17 16.83M13.987 5L12 12.415l-1.987 7.415M7 7.83L5.304 9.356C3.76 10.745 2.99 11.44 2.99 12.33c0 .89.771 1.585 2.314 2.973L7 16.83"/>
 					</svg>
-					<p className="ml-2 text-white">Visit Base64.kr</p>
+					<p className="ml-2 text-white">Base64.kr</p>
 				</div>
+			</a>
+		</div>
+		<div id="git" className="flex justify-center mb-2 space-x-2 mt-2">
+			<a href="https://github.com/JustAIGithub/AI-Code-Convert" className="text-gray cursor-pointer rounded-full">
+				<svg width="30" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path fill="#ffffff" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"/>
+				</svg>
+			</a>
+			<a href="https://twitter.com/AUDI_GUZZ" className="text-gray cursor-pointer rounded-full">
+				<svg width="28" height="28" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+				    <path fill="#ffffff" d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231l5.45-6.231Zm-1.161 17.52h1.833L7.045 4.126H5.078L17.044 19.77Z"/>
+				</svg>
 			</a>
 		</div>
 	  </div>
