f1eb888fd933cda6fbb37ee01255e52b92a57b6d
JustAIGithub <enqueueit@gmail.com>
2023-08-12 19:55:05 +0800

    add vercel web analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }: AppProps<{}>) {
   return (
     <main className="font-inter">
       <Component {...pageProps} />
+	  <Analytics />
     </main>
   );
 }
