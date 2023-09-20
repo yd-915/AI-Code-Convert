86eafc645d198293ddaf26ab750c4bf0f175937a
JustAIGithub <enqueueit@gmail.com>
2023-09-20 22:52:15 +0800

    add producthunt embed code

--- pages/index.tsx
+++ pages/index.tsx
@@ -310,6 +310,7 @@ export default function Home() {
 		  >
 		    {loading && userExplain ? 'Loading' : 'Explain'}
 		  </button>
+		  <a href="https://www.producthunt.com/products/aicodeconvert#aicodeconvert" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/follow.svg?product_id=540327&theme=light" alt="AICodeConvert - Generate&#0032;Code&#0032;or&#0032;Natural&#0032;Language&#0032;To&#0032;Another&#0032;Language&#0032;Code | Product Hunt" width="250" height="54" /></a>
 		</div>
 	  </div>
 	  <div className="pl-6 pr-6 mt-1 md:pl-20 md:pr-20 bg-[#0E1117]">
@@ -340,6 +341,7 @@ export default function Home() {
 		  		    <path fill="#ffffff" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645c0 138.72-105.583 298.558-298.558 298.558c-59.452 0-114.68-17.219-161.137-47.106c8.447.974 16.568 1.299 25.34 1.299c49.055 0 94.213-16.568 130.274-44.832c-46.132-.975-84.792-31.188-98.112-72.772c6.498.974 12.995 1.624 19.818 1.624c9.421 0 18.843-1.3 27.614-3.573c-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319c-28.264-18.843-46.781-51.005-46.781-87.391c0-19.492 5.197-37.36 14.294-52.954c51.655 63.675 129.3 105.258 216.365 109.807c-1.624-7.797-2.599-15.918-2.599-24.04c0-57.828 46.782-104.934 104.934-104.934c30.213 0 57.502 12.67 76.67 33.137c23.715-4.548 46.456-13.32 66.599-25.34c-7.798 24.366-24.366 44.833-46.132 57.827c21.117-2.273 41.584-8.122 60.426-16.243c-14.292 20.791-32.161 39.308-52.628 54.253z"/>
 		  		</svg>
 		  	</a>
+			<a href="https://www.producthunt.com/products/aicodeconvert#aicodeconvert" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/follow.svg?product_id=540327&theme=light" alt="AICodeConvert - Generate&#0032;Code&#0032;or&#0032;Natural&#0032;Language&#0032;To&#0032;Another&#0032;Language&#0032;Code | Product Hunt" width="250" height="54" /></a>
 		  </div>
 		  <div>
 			Mail: enqueueit@gmail.com
